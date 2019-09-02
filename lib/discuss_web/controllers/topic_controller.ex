73cdf8086d9a2e1509192b9051a8cd5e07106398
DucMVu <vmd0210@gmail.com>
2019-09-02 14:04:02 +0200

    show all topics on main page, create a new topic on /topics/new

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,11 +1,30 @@
 defmodule DiscussWeb.TopicController do
 	use DiscussWeb, :controller
 
-	alias Discuss.Topic
+	alias DiscussWeb.Topic
+	alias Discuss.Repo
 
-  def new(conn, params) do
+	def index(conn, _params) do
+		topics = Repo.all(Topic)
+
+		render conn, "index.html", topics: topics
+	end
+
+  def new(conn, _params) do
 		changeset = Topic.changeset(%Topic{}, %{})
 
-		render(conn, "new.html")
-  end
+		render conn, "new.html", changeset: changeset
+	end
+	
+	def create(conn, %{"topic" => topic}) do
+		changeset = Topic.changeset(%Topic{}, topic)
+
+		case Repo.insert(changeset) do
+			{:ok, post} -> 
+				# run 'mix phx.routes' in terminal to find out 'topic_path'
+				redirect conn, to: Routes.topic_path(conn, :index)
+			{:error, changeset} ->
+				render conn, "new.html", changeset: changeset
+		end
+	end
 end
