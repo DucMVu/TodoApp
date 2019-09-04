b2e81b69a80bbb742827a7f9d654aec3c5aaf839
DucMVu <vmd0210@gmail.com>
2019-09-04 17:17:14 +0200

    /auth/github works

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -2,16 +2,38 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  # pipeline :browser do
-  #   plug Ueberauth
-  #   ...
-  # end
+  alias DiscussWeb.User
+	alias Discuss.Repo
 
-  def callback(conn, params) do
-    IO.puts "--------"
-    IO.inspect(conn.assigns)
-    IO.puts "--------"
-    IO.inspect(params)
-    IO.puts "--------"
+  # might try another approach following ueberauth_example
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    user_data = %{token: auth.credentials.token, email: auth.info.email, provider: auth.provider}
+    changeset = User.changeset(%User{}, user_data)
+
+    signin(conn, changeset)
   end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect to: Routes.topic_path(conn, :index)
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect to: Routes.topic_path(conn, :index)
+    end
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
+
 end
