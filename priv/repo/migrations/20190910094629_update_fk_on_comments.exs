fc5942d4390449026d9f2ccdde93b14ce99d0b0a
DucMVu <vmd0210@gmail.com>
2019-09-10 12:29:08 +0200

    delete latest migration file to avoid migrationerror on Heroku deployment

--- priv/repo/migrations/20190910094629_update_fk_on_comments.exs
+++
@@ -1,9 +0,0 @@
-# defmodule Discuss.Repo.Migrations.UpdateFkOnComments do
-#   use Ecto.Migration
-
-#   def change do
-#     alter table("comments") do
-#       modify :topic_id, references("topics", on_delete: :delete_all), from: references("topics") 
-#     end
-#   end
-# end
