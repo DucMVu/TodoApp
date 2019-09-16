2af6f96cb70d9f9a5c9e0576ca1a3df126712d4d
Duc Vu <duc@defacto.nl>
2019-09-16 17:02:48 +0200

    ignore this

--- lib/discuss_web/models/user.ex
+++ lib/discuss_web/models/user.ex
@@ -1,14 +1,15 @@
 defmodule DiscussWeb.User do
   use DiscussWeb, :model
 
-  @derive {Jason.Encoder, only: [:nickname]}
+  # @derive {Jason.Encoder, only: [:nickname]}
+  @derive {Jason.Encoder, only: [:email]}
 
   # find table 'topic' with 'title' field
   schema "users" do
     field :email, :string
     field :provider, :string
     field :token, :string
-    field :nickname, :string
+    # field :nickname, :string
     has_many :topics, DiscussWeb.Topic
     has_many :comments, DiscussWeb.Comment
 
@@ -17,7 +18,9 @@ defmodule DiscussWeb.User do
 
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:email, :provider, :token, :nickname])
-    |> validate_required([:email, :provider, :token, :nickname])
+    # |> cast(params, [:email, :provider, :token, :nickname])
+    # |> validate_required([:email, :provider, :token, :nickname])
+    |> cast(params, [:email, :provider, :token])
+    |> validate_required([:email, :provider, :token])
   end
 end
